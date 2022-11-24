3eb75ad3fc27c62d479a01fd6358f84efcd8ef92
Pukhraj <pukhrajkhinda@gmail.com>
2022-11-24 15:43:21 +0530

    using decorators

--- api/views.py
+++ api/views.py
@@ -1,14 +1,14 @@
-from django.http import JsonResponse
-from products.models import Product
-#from django.forms.models import model_to_dict
+from django.forms.models import model_to_dict
 
+from products.models import Product
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET','POST']) #this decorator allows only methods defined by us
 def home(req):
 	model_data=Product.objects.all().order_by("?").first() #getting products randomly
 	data={}
 	#Adding model data if available
 	if model_data:
 		data=model_to_dict(model_data,fields=['id','title','content'])
-	return JsonResponse({"message":"Working Successfully","data":data})
+	return Response({"message":"Working Successfully","data":data},status=200)
--- readme.md
+++ readme.md
@@ -198,3 +198,22 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 ```
+
+## Using Decorator `@api_view([Methods])` and `Response` from `rest_framework` inside `views.py` of `api`
+
+```py
+from django.forms.models import model_to_dict
+
+from products.models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET','POST']) #this decorator allows only methods defined by us
+def home(req):
+	model_data=Product.objects.all().order_by("?").first() #getting products randomly
+	data={}
+	#Adding model data if available
+	if model_data:
+		data=model_to_dict(model_data,fields=['id','title','content'])
+	return Response({"message":"Working Successfully","data":data},status=200)
+```
